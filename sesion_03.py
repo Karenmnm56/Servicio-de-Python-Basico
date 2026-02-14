0ad30967a55957cad1cfecbccd860d6664ee00e0
Karenmnm56 <klopezllanos.a1110@gmail.com>
2026-02-13 16:11:40 -0800

    Practica 2

--- sesion_03.py
+++ sesion_03.py
@@ -17,7 +17,26 @@ mi_lista_2=["Lunes,Martes,Miercoles,Jueves,Viernes"]
 for i in mi_lista_2:
     if i != "Lunes":
         print(f"Feliz{i}!")
+#Practica 2
+# Dada la lista mi_lista_2 = ["Lunes","Martes","Miercoles","Jueves","Viernes"]
+#imprimir cada elemento de la lista 3 veces y cuando sea lunes no lo incluyas
+#Pista:usa los dos tipos loops y for en el mismo programa para logarlo
+#Resultado:
+#martes
+#miercoles
+#jueves
+#viernes
+mi_lista_3 =["Lunes","Martes","Jueves","Viernes"]
+for i in mi_lista_3:
+   if i != "":
+      print(f"{i}")
+      #while loop
+      i = 0
 
-
+      while i < 3:
+         i+=1
+         for d in mi_lista_3:
+            if d != "Lunes":
+               print(d)
 
 
